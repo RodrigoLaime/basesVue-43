bf155b70db3310bc1017b6b7d6a83d8ed9e2d377
rodrigo laime <rodrigolaime141@gmail.com>
2023-03-27 14:50:27 -0300

    finish

---
+++ Hola.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <p>Hola Mundo</p>
+    <div>{{ text }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      text: 'hola'
+    }
+  }
+};
+</script>
+
+<style>
+p {
+  color: red;
+}
+</style>
